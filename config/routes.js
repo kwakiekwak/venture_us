102a3a306f0c55ce0efd34919eecae27cb7311eb
Jack Stein <jacklsteiniv@gmail.com>
2016-01-22 16:13:12 -0800

    Added io.js file. Client connects to server

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,12 @@
 var express = require('express'),
     router  = new express.Router();
 
+// //Socket below
+// var app = require('express')();
+// var http = require('http').Server(app);
+// var io = require('socket.io')(http);
+
+
 // Require controllers.
 var welcomeController = require('../controllers/welcome');
 var usersController   = require('../controllers/users');
@@ -11,5 +17,10 @@ router.get('/', welcomeController.index);
 // users resource paths:
 router.get('/users',     usersController.index);
 router.get('/users/:id', usersController.show);
+
+// //event listener for connection (socket)
+// io.on('connection', function(socket){
+//   console.log('a user connected');
+// });
 
 module.exports = router;
---
+++ io.js
@@ -0,0 +1,9 @@
+// io.js
+
+var io = require('socket.io')();
+
+io.on('connection', function (socket) {
+  console.log('Client connected to socket.io!');
+});
+
+module.exports = io;
