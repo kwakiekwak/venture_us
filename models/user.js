77d42d92d7e21f1793dd9cc5c0689ac25434f507
La Baik <labaik237@gmail.com>
2016-01-24 15:20:28 -0800

    adding local sign up data model for user

--- models/user.js
+++ models/user.js
@@ -2,6 +2,10 @@ var mongoose = require('mongoose');
 
 module.exports = mongoose.model('User', {
 // Setting up the facebook user model schema
+  local: {
+    email: String,
+    password: String
+  },
   fb: {
     id: String,
     access_token: String,
