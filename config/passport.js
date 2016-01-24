83c995b99e9db9527fdbcb6f744bd88ad6e1f9ae
La Baik <labaik237@gmail.com>
2016-01-24 15:31:23 -0800

    adding local passport

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,7 @@
 // getting User schema model from ../models/user file
 var User = require('../models/user');
-var FacebookStrategy = require('passport-facebook').Strategy
+var FacebookStrategy = require('passport-facebook').Strategy;
+var LocalStrategy = require('passport-local').Strategy;
 
 // Serializing/Deserializing modules
 module.exports = function(passport){
@@ -16,6 +17,55 @@ module.exports = function(passport){
       });
     });
 
+    //SIGN UP LOCALLY
+
+    passport.use('local-signup', new LocalStrategy({
+        // by default, local strategy uses username and password, we will override with email
+        usernameField : 'email',
+        passwordField : 'password',
+        passReqToCallback : true // allows us to pass back the entire request to the callback
+    },
+    function(req, email, password, done) {
+
+        // asynchronous
+        // User.findOne wont fire unless data is sent back
+        process.nextTick(function() {
+
+        // find a user whose email is the same as the forms email
+        // we are checking to see if the user trying to login already exists
+        User.findOne({ 'local.email' :  email }, function(err, user) {
+            // if there are any errors, return the error
+            if (err)
+                return done(err);
+
+            // check to see if theres already a user with that email
+            if (user) {
+                return done(null, false, req.flash('signupMessage', 'That email is already taken.'));
+            } else {
+
+                // if there is no user with that email
+                // create the user
+                var newUser            = new User();
+
+                // set the user's local credentials
+                newUser.local.email    = email;
+                newUser.local.password = newUser.generateHash(password);
+
+                // save the user
+                newUser.save(function(err) {
+                    if (err)
+                        throw err;
+                    return done(null, newUser);
+                });
+            }
+
+        });
+
+        });
+
+    }));
+
+    //SIGN UP WITH FACEBOOK
     passport.use('facebook', new FacebookStrategy({
     clientID        : process.env.FACEBOOK_API_KEY,
     clientSecret    : process.env.FACEBOOK_API_SECRET,
