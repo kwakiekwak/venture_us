ab5965d5b29697ffe4e754ac4486f4e7390db93d
Jack Stein <jacklsteiniv@gmail.com>
2016-01-24 14:20:10 -0800

    Installed yelp npm, getting data back. Need to incorporate search/rendering json in a view page.

--- config/routes.js
+++ config/routes.js
@@ -54,68 +54,43 @@ router.get("/logout", function(req, res){
   res.redirect("/")
 })
 
+
 //Yelp API variables and access functions below.
+//via npm install --save yelp
 
-var oauthSignature = require('oauth-signature');
-var n = require('nonce')();
-var request = require('request');
-var qs = require('querystring');
-var _ = require('lodash');
+// Request API access: http://www.yelp.com/developers/getting_started/api_access
+var Yelp = require('yelp');
 
-/* Function for yelp call
- * ------------------------
- * set_parameters: object with params to search
- * callback: callback(error, response, body)
- */
-var request_yelp = function(set_parameters, callback) {
+var yelp = new Yelp({
+  consumer_key: 'WyY4LXaOSGB6OBlwSvkr5A',
+  consumer_secret: 'OiO0isImZ4Ebm6UGGxkyUxOpRgM',
+  token: 'mNVQwLysfi5eYcrtVIGXMGsGphE6IW5z',
+  token_secret: 'hc8Fgp9V3Gd2SIFkLAYtuB4IoxY',
+});
 
-  /* The type of request */
-  var httpMethod = 'GET';
+// See http://www.yelp.com/developers/documentation/v2/search_api
+yelp.search({ term: 'food', location: 'Montreal' })
+.then(function (data) {
+  console.log(data);
+})
+.catch(function (err) {
+  console.error(err);
+});
 
-  /* The url we are using for the request */
-  var url = 'http://api.yelp.com/v2/search';
+// See http://www.yelp.com/developers/documentation/v2/business
+yelp.business('yelp-san-francisco')
+  .then(console.log)
+  .catch(console.error);
 
-  /* We can setup default parameters here */
-  var default_parameters = {
-    location: 'San+Francisco',
-    sort: '2'
-  };
+yelp.phoneSearch({ phone: '+15555555555' })
+  .then(console.log)
+  .catch(console.error);
 
-  /* We set the require parameters here */
-  var required_parameters = {
-    oauth_consumer_key : process.env.oauth_consumer_key,
-    oauth_token : process.env.oauth_token,
-    oauth_nonce : n(),
-    oauth_timestamp : n().toString().substr(0,10),
-    oauth_signature_method : 'HMAC-SHA1',
-    oauth_version : '1.0'
-  };
+// A callback based API is also available:
+yelp.business('yelp-san-francisco', function(err, data) {
+  if (err) return console.log(error);
+  console.log(data);
+});
 
-  /* We combine all the parameters in order of importance */
-  var parameters = _.assign(default_parameters, set_parameters, required_parameters);
-
-  /* We set our secrets here */
-  var consumerSecret = process.env.consumerSecret;
-  var tokenSecret = process.env.tokenSecret;
-
-  /* Then we call Yelp's Oauth 1.0a server, and it returns a signature */
-  /* Note: This signature is only good for 300 seconds after the oauth_timestamp */
-  var signature = oauthSignature.generate(httpMethod, url, parameters, consumerSecret, tokenSecret, { encodeSignature: false});
-
-  /* We add the signature to the list of paramters */
-  parameters.oauth_signature = signature;
-
-  /* Then we turn the paramters object, to a query string */
-  var paramURL = qs.stringify(parameters);
-
-  /* Add the query string to the url */
-  var apiURL = url+'?'+paramURL;
-
-  /* Then we use request to send make the API Request */
-  request(apiURL, function(error, response, body){
-    return callback(error, response, body);
-  });
-
-};
 
 module.exports = router;
