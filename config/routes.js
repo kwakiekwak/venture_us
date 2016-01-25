5e7c03e3d4c7fb01fea3fb197030a5f4b9098134
Jack Stein <jacklsteiniv@gmail.com>
2016-01-25 12:49:59 -0800

    Foursquare API querying now working, rendering JSON. Render in EJS next

--- config/routes.js
+++ config/routes.js
@@ -1,3 +1,10 @@
+//adding dotenv up at the top
+var dotenv = require('dotenv');
+dotenv.load();
+
+client_id = process.env.CLIENT_ID,
+client_secret = process.env.CLIENT_SECRET
+
 var passport       = require('passport');
 var mongoose       = require('mongoose');
 var express        = require('express');
@@ -11,6 +18,15 @@ var ventureController = require('../controllers/venture')
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+
+//body-parser
+bodyParser   = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//here, call .env on process when you connect to the database
+//request module
+var request = require("request");
 
 // Initializing passport
 app.use(passport.initialize());
@@ -93,12 +109,41 @@ router.route('/ventures/show/:id')
 
   .get(ventureController.show)
 
-  //update a flight.
   .put(ventureController.update)
 
   .delete(ventureController.delete)
 
-//delete a flight.
+//Foursquare searching below = POST
+router.get('/search', function(req, res, next) {
+  var location = req.query.location
+  var query = req.query.keyword
+  // Printing out the content of the request!
+
+  // request("https://api.foursquare.com/v2/venues/search?near="+JSON.stringify(req.body.place.name)+"&client_id=CIFWDNLDWK55XZBRIHQ0PLN1MQUBAB135DU3HDL13EZB20L3&client_secret=GIVQE2TPTXMVP53AB0FESQRJVGPC4X1SS1VEFXOSLXPV12CE&query="+JSON.stringify(req.body.query), function(error, response, body) {
+
+    request('https://api.foursquare.com/v2/venues/search?client_id='+client_id+'&client_secret='+client_secret+'&v=20130815%20&near='+location+'%20&query='+query, function(error,response,body){
+    if(!error) {
+    //   // //EJS venues re-rerouting here.
+    //   res.render('venues', {place: req.body.place.name, query:req.body.query, venues: JSON.parse(body).response});
+    //   //above, you parse the body, and then take its response
+    //   console.log(res.venues);
+    // // }
+// //raw JSON rendering below.
+    res.send(JSON.parse(response.body));
+    console.log(location); //the location
+    console.log(req.body.query); //the query (i.e. vegan)
+    console.log(response.venues); //the response (i.e. all locations)
+    console.log(JSON.parse(response.body));
+    }
+    else {
+      res.send({venuesSearch: 'Not implemented!'}); // return some JSON
+      console.log(req.body.place.name);
+      console.log(req.body.query);
+      console.log(JSON.parse(response.body));
+    }
+  });
+
+});
 
 
 module.exports = router;
