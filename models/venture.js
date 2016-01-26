25506b732289247c88be7e68ab0208c62a173328
La Baik <labaik237@gmail.com>
2016-01-26 10:09:44 -0800

    modifying venture model

--- models/venture.js
+++ models/venture.js
@@ -3,7 +3,10 @@ var mongoose= require('mongoose')
 var ventureSchema = new mongoose.Schema({
   _id: Number,
   users: [{}], //array of user_id objects
-  choice: [{}], //array of objects. This will be a deck of cards
+  choice: [{
+    business: {},
+    vote: {} //key - user_id, value - boolean
+  }], //array of objects. This will be a deck of cards
   result: {} //final result of venture
 
 })
