5154e4f7c1dcdd96aa718420fc6a6224917c2cf2
Jack Stein <jacklsteiniv@gmail.com>
2016-01-26 11:19:26 -0800

    pushing to master and branching for photo url API calls

--- public/js/venture.js
+++ public/js/venture.js
@@ -18,3 +18,13 @@ $('.add-friend-btn').click(function(event){
     }
   })
 })
+
+// Add event listener for clicking 'submit' button for Foursquare query
+//When you click venue-search button, we want to push the top 20 options
+//for that location + query into the venture.choice array.
+//We are then going to render that array on the show page, using its
+//venue.id and venue.photos, venue.name, venue.location.address
+
+// function addToVenture() {
+//   venue.choice.push()
+// }
