8a99f1f085261b11792e3d2715657705889c7d99
Jack Stein <jacklsteiniv@gmail.com>
2016-01-26 16:55:19 -0800

    Experimenting with single-card views and an onclick() function for getting the next value

--- public/js/venture.js
+++ public/js/venture.js
@@ -1,3 +1,7 @@
+//adding dotenv up at the top
+var dotenv = require('dotenv');
+dotenv.load();
+var count = 0;
 
 // toggles the div containing friends list and search friends
 $('#show-friends-btn').click(function() {
@@ -20,13 +24,27 @@ $('.add-friend-btn').click(function(event){
 })
 
 
+//Function for clicking a button 'yes' or 'no'.
+//click yes, count 1 for that choice. Also, use counter to determine which
+//venue to show when the page reloads.
 
-// Add event listener for clicking 'submit' button for Foursquare query
-//When you click venue-search button, we want to push the top 20 options
-//for that location + query into the venture.choice array.
-//We are then going to render that array on the show page, using its
-//venue.id and venue.photos, venue.name, venue.location.address
+$('.card').click(function(event) {
+  var count = 1;
+  venue_id = limit[count].venue_id
+  //now that you have the venue_id, make a call for its picture.
+  request('https://api.foursquare.com/v2/venues/'+ venue_id + '/photos?&client_id='+client_id+'&client_secret='+client_secret+'&v=20160126', function(error,response,data){
+      if(!error) {
+        //console.log(JSON.parse(response.data));
+        //res.send(JSON.parse(response.body).response.photos.items[0]);
+        firstPhoto = JSON.parse(response.body).response.photos.items[0];
 
-// function addToVenture() {
-//   venue.choice.push()
-// }
+        //res.render('ventures/photo', {firstPhoto:firstPhoto});
+        res.render('ventures/show', {location: location, query: query, venues: venues, firstPhoto: firstPhoto})
+      }
+      else {
+         res.send({venuesSearch: 'Not implemented!'});
+
+      }
+  })
+  count+=1;
+});
