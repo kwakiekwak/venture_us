db84114a64e7bc39ac97fe77623a97774de54287
John Hwang <johnsies87@gmail.com>
2016-01-28 15:28:24 -0800

    starting modals

--- config/database.js
+++ config/database.js
@@ -4,7 +4,11 @@ var env = require('./environment');
 
 // Use different database URIs based on whether an env var exists.
 var dbUri = env.MONGOLAB_URI ||
-            'mongodb://heroku_srlhp1qr:lo08kfplt2s4llorku6a506of@ds051615.mongolab.com:51615/heroku_srlhp1qr' + env.SAFE_TITLE;
+            'mongodb://localhost/' + env.SAFE_TITLE;
+
+// change dburi to below for heroku
+// var dbUri = env.MONGOLAB_URI ||
+//             'mongodb://heroku_srlhp1qr:lo08kfplt2s4llorku6a506of@ds051615.mongolab.com:51615/heroku_srlhp1qr' + env.SAFE_TITLE;
 
 if (!env.MONGOLAB_URI) {
   // check that MongoD is running...
